85fe08d32e9b4e3df6bc2b5779382d66c840505f
jhxqy <1139648416@qq.com>
2020-05-05 20:33:16 +0800

    48 05_06

---
+++ jhx/2020-05-04/48.cpp
@@ -0,0 +1,62 @@
+/*
+请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+#include <string>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+class Solution {
+    std::unordered_map<char,int> um;
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left=0;
+        int right=0;
+        int maxvalue=0;
+        int val=0;
+        while(right<s.size()){
+            auto res=um.find(s[right]);
+            if(um.end()!=res){
+                int a=res->second;
+                while(left<=right&&left<=res->second){
+                    um.erase(s[left]);
+                    left++;
+                    val--;
+                }
+            }
+            um.insert(std::make_pair(s[right],right));
+            right++;
+            val++;
+            maxvalue=maxvalue>val?maxvalue:val;
+        }
+        return maxvalue;
+        
+    }
+};
+
+
+int main(){
+
+    cout<<Solution().lengthOfLongestSubstring("bbbbb")<<endl;
+}

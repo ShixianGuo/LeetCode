8c863f0fb5bad2ee5d291e2a2627be21dc4ac72b
奇丰 陈 <chenqifeng666@gmail.com>
2020-05-07 11:26:59 +0800

    Day7 20200506提交

---
+++ qifeng/Leet1290.hpp
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include"entity.h"
+using namespace std;
+
+/*
+给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+请你返回该链表所表示数字的 十进制值 。
+
+示例 1：
+输入：head = [1,0,1]
+输出：5
+解释：二进制数 (101) 转化为十进制数 (5)
+示例 4：
+输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+输出：18880
+示例 5：
+输入：head = [0,0]
+输出：0
+
+提示：
+链表不为空。
+链表的结点总数不超过 30。
+每个结点的值不是 0 就是 1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	int getDecimalValue(ListNode* head) {
+		ListNode* curr = head;
+		int ans = 0;
+		while (curr != nullptr) {
+			ans = (ans << 1) + curr->val;
+			curr = curr->next;
+		}
+		return ans;
+	}
+};
---
+++ qifeng/Leet23.hpp
@@ -0,0 +1,76 @@
+﻿#pragma once
+#include"entity.h"
+using namespace std;
+/*
+合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+
+示例:
+
+输入:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+输出: 1->1->2->3->4->4->5->6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	//思路1：不断找到所有头节点中最小的插入新链表中，k很大搜索很麻烦
+	//思路2：循环合并两个链表，合并两链表中不要新建链表
+	ListNode* merge(ListNode* head1, ListNode* head2) {
+		if (head1 == nullptr&&head2 == nullptr)
+			return nullptr;
+		if (head1 == nullptr)
+			return head2;
+		if (head2 == nullptr)
+			return head1;
+		//确保head1头节点小于head2
+		if (head1->val > head2->val)
+			swap(head1, head2);
+		//开始合并，将head2小的插入到当前节点的前面
+		ListNode* pre = head1;
+		ListNode* curr = head1->next;
+		while (curr != nullptr&&head2 != nullptr) {
+			if (head2->val <= curr->val) {
+				pre->next = head2;
+				head2 = head2->next;
+				pre->next->next = curr;
+
+				pre = pre->next;
+			}
+			else {
+				pre = curr;
+				curr = curr->next;
+			}
+		}
+		//剩下head2全是大的没合并
+		if (head2 != nullptr) {
+			pre->next = head2;
+		}
+		return head1;
+	}
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		int size = lists.size();
+		if (size == 0) return nullptr;
+		ListNode* ans = lists.front();
+		if (size == 1) return ans;
+		for (int i = 1; i < size; ++i) {
+			ans = merge(ans, lists[i]);
+		}
+		return ans;
+	}
+};

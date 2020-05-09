f84b413af2d2f3fece467f397218b3bc45de7f86
ylink <lfs@live.com>
2020-05-09 21:37:29 +0800

    Update 面试题38-leetcode.cpp

--- ylink/面试题38-leetcode.cpp
+++ ylink/面试题38-leetcode.cpp
@@ -11,11 +11,11 @@ public:
     bool my_next_permutation(string::iterator first, string::iterator last)
     {
         if (first == last) return false;
-        BidirIt i = last;
+        string::iterator i = last;
         if (first == --i) return false;
 
         while (true) {
-            BidirIt i1, i2;
+            string::iterator i1, i2;
 
             i1 = i;
             if (*--i < *i1) {

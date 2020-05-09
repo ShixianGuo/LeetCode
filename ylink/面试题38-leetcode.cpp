381c762e26cc3dfad0bddaa6cfddbbb73961144c
ylink <lfs@live.com>
2020-05-09 21:36:24 +0800

    Update 面试题38-leetcode.cpp

--- ylink/面试题38-leetcode.cpp
+++ ylink/面试题38-leetcode.cpp
@@ -5,7 +5,31 @@ public:
         vector<string> res;
         do {
             res.push_back(s);
-        } while(next_permutation(s.begin(), s.end()));
+        } while(my_next_permutation(s.begin(), s.end()));
         return res;
+    }
+    bool my_next_permutation(string::iterator first, string::iterator last)
+    {
+        if (first == last) return false;
+        BidirIt i = last;
+        if (first == --i) return false;
+
+        while (true) {
+            BidirIt i1, i2;
+
+            i1 = i;
+            if (*--i < *i1) {
+                i2 = last;
+                while (!(*i < *--i2))
+                    ;
+                std::iter_swap(i, i2);
+                std::reverse(i1, last);
+                return true;
+            }
+            if (i == first) {
+                std::reverse(first, last);
+                return false;
+            }
+        }
     }
 };
